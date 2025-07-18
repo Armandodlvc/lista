4322934d0e7b9c0f1af9640f843daae869aab6f3
ArmandooAr <armando.dlvc@gmail.com>
2025-07-17 21:59:22 -0700

    se agrego el botton al lado del input

--- interaccion.js
+++ interaccion.js
@@ -5,21 +5,18 @@ const tasks = document.getElementById("tasks");
 boton.addEventListener("click", () => {
   const texto = input.value.trim();
   if (texto !== "") {
-    // Línea arriba
+
     const lineaArriba = document.createElement("hr");
     lineaArriba.style.margin = "0";
     tasks.appendChild(lineaArriba);
 
-    // Contenedor para tarea y círculo
     const contenedor = document.createElement("div");
     contenedor.classList.add("task-container");
 
-    // Texto tarea
     const newtask = document.createElement("p");
     newtask.textContent = texto;
     newtask.classList.add("task");
 
-    // Círculo clickable
     const circulo = document.createElement("div");
     circulo.classList.add("terminado");
     circulo.addEventListener("click", () => {
@@ -31,7 +28,6 @@ boton.addEventListener("click", () => {
 
     tasks.appendChild(contenedor);
 
-    // Línea abajo
     const lineaAbajo = document.createElement("hr");
     lineaAbajo.style.margin = "0";
     tasks.appendChild(lineaAbajo);
